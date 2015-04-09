ada28f17907197b2eccd006ec44a857f9c39a763
Adrian <avjpl@aol.com>
2015-04-09 07:42:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple passport express app
---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'twitterAuth': {
+    'consumerKey': '',
+    'consumerSecret': '',
+    'callbackURL': 'http://localhost:8080/auth/callback'
+  }
+};
